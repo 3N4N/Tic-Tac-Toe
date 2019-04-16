54264a799d5e8f0a88be614ede6155c867cccf7b
Enan Ajmain <3nan.ajmain@gmail.com>
2019-04-16 21:00:09 +0600

    Build the window

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH (640)
+#define WINDOW_HEIGHT (480)
+
+int main(void)
+{
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
+    {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("Tic Tac Toe",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       WINDOW_WIDTH, WINDOW_HEIGHT,0);
+    if (!win)
+    {
+        printf("error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // create a renderer, which sets up the graphics hardware
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, render_flags);
+    if (!renderer)
+    {
+      printf("error creating renderer: %s\n", SDL_GetError());
+      SDL_DestroyWindow(win);
+      SDL_Quit();
+      return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(2000);
+
+    // clean up resources before exiting
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
