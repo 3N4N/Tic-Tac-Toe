1cca11411810bb5d8dd7777529d9a64fb966ea49
Enan Ajmain <3nan.ajmain@gmail.com>
2019-04-17 09:01:15 +0600

    Add mouse click support

--- main.c
+++ main.c
@@ -73,6 +73,46 @@ void drawBoard(SDL_Renderer* renderer, int arr[][3])
     return;
 }
 
+bool handleEvent(SDL_Event* event, int arr[][3])
+{
+    if (event->type == SDL_QUIT) {
+        return true;
+    }
+
+    int mouse_x, mouse_y;
+    int buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+    if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+
+        if (mouse_x < WINDOW_WIDTH / 3) {
+            if (mouse_y < WINDOW_HEIGHT / 3) {
+                arr[0][0] = X;
+            } else if (mouse_y < 2 * WINDOW_HEIGHT / 3) {
+                arr[1][0] = X;
+            } else if (mouse_y < WINDOW_HEIGHT) {
+                arr[2][0] = X;
+            }
+        } else if (mouse_x < 2 * WINDOW_WIDTH / 3) {
+            if (mouse_y < WINDOW_HEIGHT / 3) {
+                arr[0][1] = X;
+            } else if (mouse_y < 2 * WINDOW_HEIGHT / 3) {
+                arr[1][1] = X;
+            } else if (mouse_y < WINDOW_HEIGHT) {
+                arr[2][1] = X;
+            }
+        } else if (mouse_x < WINDOW_WIDTH) {
+            if (mouse_y < WINDOW_HEIGHT / 3) {
+                arr[0][2] = X;
+            } else if (mouse_y < 2 * WINDOW_HEIGHT / 3) {
+                arr[1][2] = X;
+            } else if (mouse_y < WINDOW_HEIGHT) {
+                arr[2][2] = X;
+            }
+        }
+
+    }
+}
+
 int main(int argc, int *argv[])
 {
     int arr[3][3] = { { 2, 0, 1 },
@@ -112,9 +152,7 @@ int main(int argc, int *argv[])
     while (!quit) {
 
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
+            quit = handleEvent(&event, arr);
         }
 
         drawBoard(renderer, arr);
