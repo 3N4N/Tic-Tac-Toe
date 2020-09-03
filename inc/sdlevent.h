9717308d624275b898da00bede20463e6b7e1529
Enan Ajmain <3nan.ajmain@gmail.com>
2020-09-03 16:32:21 +0600

    Customize start screen for single player mode

--- inc/sdlevent.h
+++ inc/sdlevent.h
@@ -1,7 +1,6 @@
 #ifndef SDL_EVENT_H
 #define SDL_EVENT_H
 
-int switchPlayer(int current_player);
 bool handleEvent(SDL_Event* event, int arr[]);
 bool handleStartScreenEvent(SDL_Event* event, int arr[]);
 bool handleGameScreenEvent(SDL_Event* event, int arr[]);
